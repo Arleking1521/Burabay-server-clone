04791b4307a3ad385281dc0e22744bd1779c0b8a
Arleking <Abdukarim600@gmail.com>
2024-04-12 12:31:10 +0500

    update length of title

--- src/news/models.py
+++ src/news/models.py
@@ -5,7 +5,7 @@ from logRegisPages.models import CustomUser
 
 class Post(models.Model):
     author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, verbose_name='Автор')
-    title = models.CharField(max_length = 128, verbose_name='Заголовок')
+    title = models.CharField(max_length = 1000, verbose_name='Заголовок')
     date = models.DateTimeField(default=timezone.now, verbose_name='Дата публикации')
     content = models.TextField(verbose_name='Контент')
 
