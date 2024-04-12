f824ed40355e8dd56a989f15f7cad07d4efaf8f1
Arleking <Abdukarim600@gmail.com>
2024-04-12 16:29:19 +0500

    add new pages

--- src/minZdrav/urls.py
+++ src/minZdrav/urls.py
@@ -19,40 +19,51 @@ urlpatterns = [
     
 ]
 urlpatterns += i18n_patterns(
-    path('news/', include('news.urls')),
-    path('new-news/', post_new, name = 'new_new'),
-    path('med-services/', include('medServices.urls')),
-    path('rules/', include('info_page.urls')),
-    path('viewSovet/', include('viewSovet.urls')),
-    path('contacts/', include('contact_pages.urls')),
-    path('histories/', include('history_legends.urls')),
-    path('ceo-info/', include('ceoInfo.urls')),
-    path('vacancies/', include('vacancies.urls')),
-    path('organization-structure/', include('orgStruct.urls')),
-    path('rights-acts/', include('ProvActs.urls')),
-    path('workers-info/', include('workersInfo.urls')),
-    path('anticorruption/', include('antiCorruptions.urls')),
-    path('about/', include('aboutPage.urls')),    
-    path('reviews/', include('reviewsBlog.urls')),
-    path('advertisement/', include('advertisement.urls')),
-    path('appeal/', include('appeal.urls')),
-    path('login/', user_login, name = 'login'),
-    path('logout/', user_logout, name = 'logout'),
-    path('registration/', registration, name = 'registr'),
-    path('password-reset/', CustomPasswordResetView.as_view(), name='password_reset'),
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', CustomPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('news/', include('news.urls')), #Новости
+    path('new-news/', post_new, name = 'new_new'), #Новые новости
+    path('med-services/', include('medServices.urls')), #Медицинские услуги (пока что)
+    path('rules/', include('info_page.urls')), #Правила санатория
+    path('viewSovet/', include('viewSovet.urls')), #Наблюдательный совет
+    path('contacts/', include('contact_pages.urls')), #контакты
+    path('histories/', include('history_legends.urls')), #История и легенды
+    path('ceo-info/', include('ceoInfo.urls')), #О руководителе
+    path('vacancies/', include('vacancies.urls')), #Вакансии
+    path('organization-structure/', include('orgStruct.urls')), #Организационная структура
+    path('rights-acts/', include('ProvActs.urls')), #Правовые акты
+    path('workers-info/', include('workersInfo.urls')), #Информация о сотрудниках
+    path('anticorruption/', include('antiCorruptions.urls')), #Противодействие коррупции
+    path('about/', include('aboutPage.urls')),    #О НИИ
+    path('reviews/', include('reviewsBlog.urls')), #Отзывы
+    path('advertisement/', include('advertisement.urls')), #Объявления
+    path('appeal/', include('appeal.urls')), #Обращения
+    path('login/', user_login, name = 'login'), #Авторизация
+    path('logout/', user_logout, name = 'logout'), #Выход
+    path('registration/', registration, name = 'registr'), #Регистрация
+    path('password-reset/', CustomPasswordResetView.as_view(), name='password_reset'), #Забыли пароль
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'), #Новый пароль
+    path('reset/<uidb64>/<token>/', CustomPasswordResetConfirmView.as_view(), name='password_reset_confirm'), #
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-    path('download/<str:file_name>/', download_file, name='download_file'),
-    path('activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/', activate, name='activate'),
+    path('download/<str:file_name>/', download_file, name='download_file'), #Скачивание файлов
+    path('activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/', activate, name='activate'), #страница активации аккаунта
 
-    path('gobmp/', TemplateView.as_view(template_name='dopPages/gobmp.html'), name='gobmp'),
-    path('scientific-developments/', TemplateView.as_view(template_name='dopPages/scientificDev.html'), name='sci-dev'),
-    path('plans/', TemplateView.as_view(template_name='dopPages/plans.html'), name='plans'),
-    path('quality-management-standard/', TemplateView.as_view(template_name='dopPages/managment_standart.html'), name='managment_standart'),
-    path('strategic-development/', TemplateView.as_view(template_name='dopPages/strategic_development.html'), name='srategic-dev'),
-    path('quality-managment-standart/', TemplateView.as_view(template_name='dopPages/quality_managment.html'), name='QMStand'),
-    
+    path('gobmp/', TemplateView.as_view(template_name='dopPages/gobmp.html'), name='gobmp'), #получение услуг по ГОБМП
+    path('scientific-developments/', TemplateView.as_view(template_name='dopPages/scientificDev.html'), name='sci-dev'), #Научные достижения
+    path('plans/', TemplateView.as_view(template_name='dopPages/plans.html'), name='plans'), #Планы
+    path('quality-management-standard/', TemplateView.as_view(template_name='dopPages/managment_standart.html'), name='managment_standart'), #Стандарты менеджмента качества
+    path('strategic-development/', TemplateView.as_view(template_name='dopPages/strategic_development.html'), name='srategic-dev'), #Стратегическое развитие
+    path('ceo-blog/', TemplateView.as_view(template_name='dopPages/blogCEO.html'), name='blogCEO'),
+    path('achievments/', TemplateView.as_view(template_name='dopPages/achievments.html'), name='achievments'),
+    path('people/', TemplateView.as_view(template_name='dopPages/NIIPeople.html'), name='people'),
+    path('doctors/', TemplateView.as_view(template_name='dopPages/doctors.html'), name='doctors'),
+    path('teachers/', TemplateView.as_view(template_name='dopPages/teachers.html'), name='teachers'),
+    path('uvo/', TemplateView.as_view(template_name='dopPages/UVO.html'), name='uvo'),
+    path('schedule-of-childrens-shifts/', TemplateView.as_view(template_name='dopPages/kidsSchedule.html'), name='kidsSchedule'),
+    path('media-galery/', TemplateView.as_view(template_name='dopPages/mediagallery.html'), name='mediagallery'),
+    path('strategic-partners/', TemplateView.as_view(template_name='dopPages/strategicPartners.html'), name='strategic_partners'),
+    path('ethics-regulations/', TemplateView.as_view(template_name='dopPages/ethicsRegulations.html'), name='ethicsRegulations'),
+    path('labor-protection/', TemplateView.as_view(template_name='dopPages/laborProtection.html'), name='laborProtection'),
+    path('leisure/', TemplateView.as_view(template_name='dopPages/leisure.html'), name='leisure'),
+    path('transit/', TemplateView.as_view(template_name='dopPages/transit.html'), name='transit'),
 )
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
