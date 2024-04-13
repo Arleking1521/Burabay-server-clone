a3e9fd8db4d2ad9c71034514cfdb8fbb3eb0db5a
Arleking <Abdukarim600@gmail.com>
2024-04-13 16:40:46 +0500

    update people pages

--- src/minZdrav/urls.py
+++ src/minZdrav/urls.py
@@ -53,9 +53,7 @@ urlpatterns += i18n_patterns(
     path('strategic-development/', TemplateView.as_view(template_name='dopPages/strategic_development.html'), name='srategic-dev'), #Стратегическое развитие
     path('ceo-blog/', include('ceoblog.urls')),
     path('achievments/', TemplateView.as_view(template_name='dopPages/Achievments.html'), name='achievments'),
-    path('people/', TemplateView.as_view(template_name='dopPages/NIIPeople.html'), name='people'),
-    path('doctors/', TemplateView.as_view(template_name='dopPages/doctors.html'), name='doctors'),
-    path('teachers/', TemplateView.as_view(template_name='dopPages/teachers.html'), name='teachers'),
+    path('people/', include('people.urls'), name='people'),
     path('uvo/', TemplateView.as_view(template_name='dopPages/UVO.html'), name='uvo'),
     path('schedule-of-childrens-shifts/', TemplateView.as_view(template_name='dopPages/kidsSchedule.html'), name='kidsSchedule'),
     path('media-galery/', TemplateView.as_view(template_name='dopPages/mediagallery.html'), name='mediagallery'),
---
+++ src/people/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.people_list, name = 'people'),
+    path('doctors/', views.doctor_list, name = 'doctors'),
+    path('teachers/', views.teacher_list, name = 'teachers'),
+    # path('<int:pid>/', views.post_detail, name = 'new_detail'),
+    # path('new/', views.post_new, name='new_new'),
+    # path('<int:pid>/edit/', views.post_edit, name='new_edit'),
+]
