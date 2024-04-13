4c0ff5adeee7140c7709f74591989dd51b6429bf
Arleking <Abdukarim600@gmail.com>
2024-04-13 17:24:48 +0500

    update urls

--- src/people/urls.py
+++ src/people/urls.py
@@ -6,6 +6,4 @@ urlpatterns = [
     path('doctors/', views.doctor_list, name = 'doctors'),
     path('teachers/', views.teacher_list, name = 'teachers'),
     path('<int:pid>/', views.people_detail, name = 'person_detail'),
-    # path('new/', views.post_new, name='new_new'),
-    # path('<int:pid>/edit/', views.post_edit, name='new_edit'),
 ]
